cab8784910a8cf566b494cc12c3a4a3529bc51ea
MagnusSandbergITHS <magnus.sandberg@iths.se>
2022-11-16 12:59:47 +0100

    Update everyloop.sql
    4 klara

--- SQLÖvningarMagnus/everyloop.sql
+++ SQLÖvningarMagnus/everyloop.sql
@@ -1,5 +1,7 @@
 --USE everyloop;
+
 --select * into Elements2 from Elements;
+
 --select Episode, Title from GameOfThrones;
 
 --select FORMAT(Season, 'S0#') AS 'Season', FORMAT(Episode, 'E0#') AS 'Episode' from GameOfThrones;
@@ -15,4 +17,18 @@
 --UPDATE Users2 
 --SET UserName = LOWER(CONCAT(SUBSTRING(FirstName, 1, 2), SUBSTRING(LastName, 1, 2)));
 
-SELECT * FROM Users2;
+--UPDATE Airports2
+--SET [Time] = '-'
+--WHERE [Time] IS NULL;
+
+--UPDATE Airports2
+--SET [DST] = '-'
+--WHERE [DST] IS NULL;
+
+--SELECT * FROM Airports2;
+
+--DELETE FROM Elements2
+-- WHERE [Name] IN ('Erbium', 'Helium', 'Nitrogen', 'Platinum', 'Selenium') or
+-- [Name] LIKE 'd%' or [name] like 'k%' or [name] like 'm%' or [name] like 'o%' or [name] like'u%';
+
+--SELECT * FROM Elements2;
