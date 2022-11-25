03bbf162d43f59f2c78e5b8827cf459b1d0a7a03
MagnusSandbergITHS <magnus.sandberg@iths.se>
2022-11-25 12:20:50 +0100

    Update everyloop.sql
    asd

--- SQLÖvningarMagnus/everyloop.sql
+++ SQLÖvningarMagnus/everyloop.sql
@@ -234,15 +234,7 @@
 --4: Vilket är det högsta antal artister som finns på en enskild spellista?
 --5. Vilket är det genomsnittliga antalet artister per spellista?
 
-DECLARE @playlist VARCHAR(max) = 'Heavy Metal Classic';
-
-SELECT @playlist FROM music.playlists;
-SELECT * FROM music.playlists;
-SELECT * FROM music.playlist_track;
-SELECT * FROM music.tracks;
-SELECT * FROM music.albums;
-SELECT * FROM music.artists;
---Genre	Artist	Album	Track	Length	Size	Composer
+--DECLARE @playlist VARCHAR(max) = 'Heavy Metal Classic';
 
 --SELECT * INTO music.tracks2 FROM music.tracks;
 
@@ -250,13 +242,147 @@ SELECT * FROM music.artists;
 --SET Composer = '-'
 --WHERE Composer IS NULL; 
 
-SELECT genres.Name AS Genre, artists.Name AS Artist, albums.Title AS Album, tracks.Name AS Track, 
-RIGHT(CONVERT(CHAR(8),DATEADD(second, (Milliseconds / (1000)), 0),108),5) AS Length,
-CONCAT(ROUND(CONVERT(float, Bytes / CONVERT(float, 1048576)), 1), ' MiB') AS Size, Composer
-FROM music.tracks
-JOIN music.albums ON music.tracks.AlbumId = music.albums.AlbumId
-JOIN music.artists ON music.albums.Artistid = music.artists.ArtistId
-JOIN music.genres ON music.tracks.GenreId = music.genres.GenreId
-JOIN music.playlist_track ON music.tracks.TrackId = music.playlist_track.TrackId
-WHERE PlaylistId = 17;
+--SELECT genres.Name AS Genre, artists.Name AS Artist, albums.Title AS Album, tracks.Name AS Track, 
+--RIGHT(CONVERT(CHAR(8),DATEADD(second, (Milliseconds / (1000)), 0),108),5) AS Length,
+--CONCAT(ROUND(CONVERT(float, Bytes / CONVERT(float, 1048576)), 1), ' MiB') AS Size, Composer
+--FROM music.tracks
+--JOIN music.albums ON music.tracks.AlbumId = music.albums.AlbumId
+--JOIN music.artists ON music.albums.Artistid = music.artists.ArtistId
+--JOIN music.genres ON music.tracks.GenreId = music.genres.GenreId
+--JOIN music.playlist_track ON music.tracks.TrackId = music.playlist_track.TrackId
+--WHERE PlaylistId = 17;
 
+--SELECT @playlist FROM music.playlists;
+--SELECT * FROM music.playlists;
+--SELECT * FROM music.playlist_track;
+--SELECT * FROM music.tracks;
+--SELECT * FROM music.albums;
+--SELECT * FROM music.artists;
+
+--1: Av alla audiospår, vilken artist har längst total speltid?
+
+--select artists.Name as arstist, sum(Milliseconds) as length 
+--from music.tracks
+--join music.albums on music.tracks.AlbumId = music.albums.AlbumId
+--join music.artists on music.albums.ArtistId = music.artists.ArtistId
+--join music.genres on music.tracks.GenreId = music.genres.GenreId
+--where music.genres.Name not in ('Science Fiction', 'TV Shows', 'Sci Fi & Fantasy', 'Drama', 'Comedy')
+--group by artists.Name
+--order by length DESC
+
+--2: Vad är den genomsnittliga speltiden på den artistens låtar?
+
+--SELECT art.Name, RIGHT(CONVERT(CHAR(8),DATEADD(second, (AVG(Milliseconds) / (1000)), 0),108),5) AS Length
+--FROM music.tracks AS t
+--JOIN music.albums AS a ON t.AlbumId = a.AlbumId
+--JOIN music.artists AS art ON art.ArtistId = a.ArtistId
+--WHERE art.Name LIKE 'Iron Maiden'
+--GROUP BY art.Name
+
+--3: Vad är den sammanlagda filstorleken för all video?
+
+--SELECT SUM(ROUND(CONVERT(float, Bytes / CONVERT(float, 1073741824)), 2)) AS SumSizeGiB 
+--FROM music.tracks
+--JOIN music.albums ON music.tracks.AlbumId = music.albums.AlbumId
+--JOIN music.artists ON music.albums.ArtistId = music.artists.ArtistId
+--WHERE music.tracks.MediaTypeId = 3;
+
+--4: Vilket är det högsta antal artister som finns på en enskild spellista?
+
+--SELECT PlaylistId, COUNT(DISTINCT music.artists.ArtistId) AS CountArtists 
+--FROM music.tracks
+--JOIN music.playlist_track ON music.tracks.TrackId = music.playlist_track.TrackId
+--JOIN music.albums ON music.tracks.AlbumId = music.albums.AlbumId
+--JOIN music.artists on music.albums.ArtistId = music.artists.ArtistId
+--WHERE music.playlist_track.PlaylistId NOT IN (2, 3, 4, 6, 7, 9, 10)
+--GROUP BY PlaylistId
+--ORDER BY CountArtists DESC;
+
+--5: Vilket är det genomsnittliga antalet artister per spellista?
+
+--CREATE VIEW vAvgArtist
+--AS
+--SELECT PlaylistId, COUNT(DISTINCT music.artists.ArtistId) AS CountArtists 
+--FROM music.tracks
+--JOIN music.playlist_track ON music.tracks.TrackId = music.playlist_track.TrackId
+--JOIN music.albums ON music.tracks.AlbumId = music.albums.AlbumId
+--JOIN music.artists on music.albums.ArtistId = music.artists.ArtistId
+--WHERE music.playlist_track.PlaylistId NOT IN (2, 3, 4, 6, 7, 9, 10)
+--GROUP BY PlaylistId;
+
+--SELECT AVG(CountArtists) FROM vAvgArtist;
+
+-----------------------------------------------------------------------------------------------
+
+--1: Ta ut (select) en rad för varje (unik) period i tabellen ”Elements” med följande kolumner: 
+--”period”, ”from” med lägsta atomnumret i perioden, ”to” med högsta atomnumret i perioden, 
+--”average isotopes” med genomsnittligt antal isotoper visat med 2 decimaler, ”symbols” 
+--med en kommaseparerad lista av alla ämnen i perioden.
+
+--SELECT 
+--DISTINCT [Period], 
+--MIN(Number) AS [from],
+--MAX(Number) AS [to],
+--ROUND(AVG(CONVERT(float, Stableisotopes)), 2) AS [average isotopes],
+--STRING_AGG(Symbol, ', ') AS [symbols]
+--FROM [Elements]
+--GROUP BY Period;
+
+--SELECT * FROM Elements;
+
+--2: För varje stad som har 2 eller fler kunder i tabellen Customers, ta ut (select) följande 
+--kolumner: ”Region”, ”Country”, ”City”, samt ”Customers” som anger hur många kunder som 
+--finns i staden.
+
+--SELECT Region, Country, City, COUNT(City) AS Customers
+--FROM company.customers
+--GROUP BY City, Region, Country
+--HAVING COUNT(City) > 1;
+
+--SELECT * 
+--FROM company.customers
+--ORDER BY City;
+
+--3: Skapa en varchar-variabel och skriv en select-sats som sätter värdet: 
+--”Säsong 1 sändes från april till juni 2011. Totalt sändes 10 avsnitt, som i genomsnitt 
+--sågs av 2.5 miljoner människor i USA.”, följt av radbyte/char(13), följt av 
+--”Säsong 2 sändes …” osv. När du sedan skriver (print) variabeln till messages ska du 
+--alltså få en rad för varje säsong enligt ovan, med data sammanställt från tabellen GameOfThrones. 
+--Tips: Ange ’sv’ som tredje parameter i format() för svenska månader.
+
+--DECLARE @GameOfThrones AS varchar(max)
+--SELECT @GameOfThrones = ''
+
+--SELECT 
+--@GameOfThrones +=  
+--'Säsong ' + CONVERT(VARCHAR, Season) + ' sändes från ' + 
+--FORMAT(MIN([Original air date]), 'MMMM', 'sv') + ' till ' + 
+--FORMAT(MAX([Original air date]), 'MMMM yyyy', 'sv') + '. Totalt sändes ' + 
+--CONVERT(VARCHAR, MAX(EpisodeInSeason)) + ' avsnitt, som i genomsnitt sågs av ' +
+--CONVERT(VARCHAR, ROUND(AVG([U.S. viewers(millions)]), 2)) + ' miljoner människor i USA.' + CHAR(13)
+--FROM GameOfThrones
+--GROUP BY Season
+--PRINT @GameOfThrones;
+
+--4: Ta ut (select) alla användare i tabellen ”Users” så du får tre kolumner: ”Namn” som har 
+--fulla namnet; ”Ålder” som visar hur många år personen är idag (ex. ’45 år’); ”Kön” som visar 
+--om det är en man eller kvinna. Sortera raderna efter för- och efternamn.
+
+--SELECT
+--CONCAT(FirstName, ' ', LastName) AS Namn, 
+--CONCAT(DATEDIFF(year, SUBSTRING([ID], 1, 6), GETDATE()), + ' år') AS Ålder, 
+--CASE
+--    WHEN SUBSTRING([ID], 10, 1) %2 = 0 THEN 'Kvinna'
+--    ELSE 'Man'
+--END AS Kön
+--FROM Users
+
+--5: Ta ut en lista över regioner i tabellen ”Countries” där det för varje region framgår 
+--regionens namn, antal länder i regionen, totalt antal invånare, total area, befolkningstätheten 
+--med 2 decimaler, samt spädbarnsdödligheten per 100.000 födslar avrundat till heltal.
+
+
+
+--6: Från tabellen ”Airports”, gruppera per land och ta ut kolumner som visar: land, 
+--antal flygplatser (IATA-koder), antal som saknar ICAO-kod, samt hur många procent av 
+--flygplatserna i varje land som saknar ICAO-kod.
